0a46b7e691ae69bec83ad49f9d91a43c1004c3a5
gAbhishekS <abhishek.g@razorpay.com>
2024-03-05 09:46:01 +0530

    added all basics tasks

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	//First program Hello World
 	fmt.Println("hello world")
+
+	defer fmt.Println("defer statement executes at last.")
+
+	//Variables and data type
+	var isBool, isTrue bool = true, false
+	fmt.Println("isBool:", isBool)
+	fmt.Println("isTrue:", isTrue)
+
+	withoutVar := 14
+	fmt.Println("short assignment:", withoutVar)
+
+	runeVar := 'A'
+	fmt.Println("ASCII value for A:", runeVar)
+
+	//For loop
+	for i := 0; i < 3; i++ {
+		fmt.Println("number", i)
+	}
+
+	//while loop
+	sum := 1
+	for sum < 6 {
+		sum++
+	}
+	fmt.Println("sum", sum)
 }
